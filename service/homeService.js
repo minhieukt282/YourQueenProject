8f01a6ddcf116f6cef59e21c876786dea0a89fb1
hoangtran2004 <tranhuyhoang58825@gmail.com>
2022-10-25 17:27:58 +0700

    huyhoang 5:27 25/10/22

--- service/homeService.js
+++ service/homeService.js
@@ -1,9 +1,22 @@
 const CONNECTION = require('../model/connection')
+const Connection = require("../../../gitTestProject/model/connectDatabase");
 
 CONNECTION.connecting()
 
 class HomeService {
+    static createNewAccount(account){
+        let connection=Connection.getConnection();
+        return new Promise((resolve, reject) => {
+            connection.query(`insert into account(username,password,role_id,status_id) values ('${account.username}','${account.password}',${account.role_id=3},${account.status_id=2})`,(err,account)=>{
+                if (err){
+                    reject(err)
+                }else {
+                    resolve(account)
+                }
+            })
 
+        })
+    }
 }
 
 module.exports = new HomeService()
